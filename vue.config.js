ce81f7b749fb4440b4d3ccdf24c1b1569e6a4b6f
liangheng <291294950@qq.com>
2022-03-05 14:03:00 +0800

    bpmn-jeecgboot

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  lintOnSave: false
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: false,
+  productionSourceMap: false,
+  devServer: {
+    port: 8080,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+  },
 }
